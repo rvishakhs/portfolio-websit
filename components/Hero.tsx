054ca9c4561d751e5f0a12922c151c140d69a7bd
rvishakhs <rvishakhs@gmail.com>
2022-10-18 23:48:03 +0100

    hero section finished

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import Background from './Background'
@@ -18,18 +19,37 @@ function Hero({}: Props) {
 
 
   return (
-    <div className='h-screen flex flex-col space-y-8 items-center justify-center'>
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center max-w-7xl mx-auto '>
         <Background />
         <img 
             src='https://avatars.githubusercontent.com/u/56556101?v=4'
             className='relative rounded-full h-32 w-32 mx-auto object-cover'
         />
-        <h2 className='text-4xl font-semibold'>
-            <span>
-                {text}
-            </span>
-                <Cursor cursorColor='#FF0000'/>
-        </h2>
+
+        <h2  className='text-gray-500 font-semibold pb-2 tracking-[15px] text-sm uppercase '>Web developer </h2>
+            <h2 className='text-4xl font-semibold px-2'>
+                <span className='mr-3'>
+                    {text}
+                </span>
+                    <Cursor cursorColor='#FF0000'/>
+            </h2>
+
+            <div className='pt-4 z-20'>
+                <Link href="#hero">
+                    <button className='herobtn'>About</button>
+                </Link>
+                <Link href="#Expereince">
+                    <button className='herobtn'>Experience</button>
+                </Link>
+                <Link href='#skills'>
+                    <button className='herobtn'>Skills</button>
+                </Link>
+                <Link href="#projects">
+                    <button className='herobtn'>Projects</button>
+                </Link>
+            </div>
+        
+
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] h-screen text-white snap-y  snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] h-screen text-white snap-y  snap-mandatory  overflow-scroll z-0">
       <Head>
         <title>Visakh's Portfolio</title>
       </Head>
