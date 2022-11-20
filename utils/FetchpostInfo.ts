d687c9b4499b6d3fcb10fbe6066563fff125f278
rvishakhs <rvishakhs@gmail.com>
2022-11-20 21:14:58 +0000

    bug fixing round 1

--- utils/FetchpostInfo.ts
+++ utils/FetchpostInfo.ts
@@ -3,14 +3,11 @@ import {postInfo} from "../typings"
 
 export const FetchpostInfo = async() => {
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/getpostInfo`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getpostInfo`);
 
     const data = await res.json()
 
     const postInfo: postInfo[] = data.postinfo
-
-    console.log(postInfo);
-    
 
     return postInfo
     
--- utils/Fetchskills.ts
+++ utils/Fetchskills.ts
@@ -3,7 +3,7 @@ import {skill} from "../typings"
 
 export const Fetchskills = async() => {
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/getSkills`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`);
 
     const data = await res.json()
 
